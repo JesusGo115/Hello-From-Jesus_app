d79ec6d36a1a8a19a3184c5c93984780cb395e3c
viksMVP <PS4rules>
2018-10-01 00:54:13 -0700

    Test

---
+++ app/src/main/java/com/example/gmjes/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.gmjes.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView)findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.colorPrimary));
+            }
+        });
+    }
+    public native String stringFromJNI();
+}
